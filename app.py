61c2842203154b529d0f2ee40f54af74ec945663
samiahmednew <112119255+samiahmedalfasly@users.noreply.github.com>
2024-02-28 09:48:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def youtube_views(num_page=1):
 
 @app.route('/vote_url', methods = ['POST'])
 def vote_url():
-    from vote_new import run_vote
+    from vote import run_vote
     run_vote()
     return "url data: "
 
